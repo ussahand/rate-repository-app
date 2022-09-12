c43f3b0e82b2649b5c975612d4c8b84c7979904f
Mahdi A <sahandprojects@gmail.com>
2022-09-12 10:23:40 -0700

    first initialized expo

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello React-Native-Expo!</Text>
+      <Text>Try your best</Text>
+      <Text>ّI am ready now...</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
